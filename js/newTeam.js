a0f448d0884d5bc62514640d80f7c24ea1c0cf54
nicosiimon <nico.rubach@web.de>
2019-10-31 15:04:22 +0100

    Added image upload capabilities

--- js/newTeam.js
+++ js/newTeam.js
@@ -1,3 +1,5 @@
+let newTeamContext;
+
 class PageNewTeam {
 
   static id = 0;
@@ -5,6 +7,7 @@ class PageNewTeam {
 
   constructor(app) {
     this._app = app;
+    newTeamContext = this;
   }
 
   async show() {
@@ -19,6 +22,15 @@ class PageNewTeam {
     alertBox.hidden = true;
     this.players = $("#players");
     $("#addNewPlayer").on("click", this.submitPlayer);
+
+    // Setup the dnd listeners.
+    this.dropZone = document.getElementById('tvarea');
+    this.dropZone.addEventListener('dragover', this.handleDragOver, false);
+    this.dropZone.addEventListener('drop', this.handleFileSelect, false);
+
+    $("#inputfile").change(function(){
+        newTeamContext.readURL(this);
+    });
   }
 
   submitPlayer() {
@@ -42,38 +54,71 @@ class PageNewTeam {
     PageNewTeam.id++;
 
     // Generate card for saved player
-    let player = $('<div class="card col-sm-4">' + 
-    '<div class="card-body">' + 
-        '<div class="row">' + 
-            '<div class="col-lg">' + 
-              '<span>Gamertag: </span>' + 
+    let player = $('<div class="card col-sm-4">' +
+    '<div class="card-body">' +
+        '<div class="row">' +
+            '<div class="col-lg">' +
+              '<span>Gamertag: </span>' +
             '</div>' +
-            '<div class="col-lg">' + 
-            '<b>' + $("#newPlayerName").val() +'</b>' + 
-            '</div>' + 
-          '</div>' + 
-          '<div class="row">' + 
-              '<div class="col-lg">' + 
-                  '<span>Skill rating: </span>' + 
+            '<div class="col-lg">' +
+            '<b>' + $("#newPlayerName").val() +'</b>' +
+            '</div>' +
+          '</div>' +
+          '<div class="row">' +
+              '<div class="col-lg">' +
+                  '<span>Skill rating: </span>' +
               '</div>' +
               '<div class="col-lg">' +
-                  '<b>' + $("#newPlayerSR").val() + '</b>' + 
-              '</div>' + 
-          '</div>' + 
+                  '<b>' + $("#newPlayerSR").val() + '</b>' +
+              '</div>' +
+          '</div>' +
           '<div class="row">' +
-              '<div class="col-lg">' + 
-                  '<span>Captain? </span>' + 
-              '</div>' + 
+              '<div class="col-lg">' +
+                  '<span>Captain? </span>' +
+              '</div>' +
               '<div class="col-lg">' +
                   '<b>' + $("#newPlayerCaptain").val() + '</b>' +
               '</div>' +
           '</div>' +
       '</div>' +
   '</div>').appendTo("#players");
-    
+
     // reset newPlayer Card
     $("#newPlayerName").val("")
     $("#newPlayerSR").val("")
     $("#newPlayerCaptain").val("No")
   }
+
+  handleFileSelect(evt) {
+        evt.stopPropagation();
+        evt.preventDefault();
+        var reader = new FileReader();
+        var inputFile = evt.dataTransfer.files[0];
+        reader.onload = function(e){
+            $('#thumbnail').attr('src', e.target.result);
+            $('#thumbnail').attr('style', "display=block;");
+            document.getElementById('inputfilelabel').innerHTML = inputFile.name;
+        }
+        document.querySelector('#inputfile').files = evt.dataTransfer.files;
+        reader.readAsDataURL(inputFile);
+    }
+    handleDragOver(evt) {
+        evt.stopPropagation();
+        evt.preventDefault();
+        evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+    }
+
+    readURL(input){
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function(e){
+                $('#thumbnail').attr('src', e.target.result);
+                $('#thumbnail').attr('style', "display=block;");
+                document.getElementById('inputfilelabel').innerHTML = input.files[0].name;
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+
 }
