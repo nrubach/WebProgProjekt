8b74f0f874eafc97b7c960d0861a9ef379273d24
Theodor Gauguin Houghton-Larsen <tegahola@gmail.com>
2019-10-31 13:04:43 +0100

    adds new player functionality. integrates firebase. writes new players to database and in the view into a card

--- js/newTeam.js
+++ js/newTeam.js
@@ -1,5 +1,6 @@
 class PageNewTeam {
 
+  static id = 0;
   faultyElement;
 
   constructor(app) {
@@ -17,15 +18,62 @@ class PageNewTeam {
     this.alertBox = $("#alertBox");
     alertBox.hidden = true;
     this.players = $("#players");
-    console.log("PageNewTeam created");
-    $("#addNewPlayer").on("click", this.setAlertMessage());
+    $("#addNewPlayer").on("click", this.submitPlayer);
   }
 
-  setAlertMessage() {
-    $("#faultyElement").innerHTML = this.faultyElement;
-    $("#alertBox").hidden = false;
-    firebase.database().ref('teams/' + "contenders").set({
-      name: "Contenders Europe"
+  submitPlayer() {
+    if ($("#newPlayerName").val().length < 2) {
+      alertBox.hidden = false;
+      $("#faultyElement").html("Name");
+      return;
+    }
+    if ($("#newPlayerSR").val() < 0 || $("#newPlayerSR").val() > 5000) {
+      alertBox.hidden = false;
+      $("#faultyElement").html("Skill Rating");
+      return;
+    }
+
+    database.ref('teams/' + "players/" + PageNewTeam.id).set({
+      name: $("#newPlayerName").val(),
+      sr: $("#newPlayerSR").val(),
+      captain: $("#newPlayerCaptain").val()
     });
+    console.log("Written to database");
+    PageNewTeam.id++;
+
+    // Generate card for saved player
+    let player = $('<div class="card col-sm-4">' + 
+    '<div class="card-body">' + 
+        '<div class="row">' + 
+            '<div class="col-lg">' + 
+              '<span>Gamertag: </span>' + 
+            '</div>' +
+            '<div class="col-lg">' + 
+            '<b>' + $("#newPlayerName").val() +'</b>' + 
+            '</div>' + 
+          '</div>' + 
+          '<div class="row">' + 
+              '<div class="col-lg">' + 
+                  '<span>Skill rating: </span>' + 
+              '</div>' +
+              '<div class="col-lg">' +
+                  '<b>' + $("#newPlayerSR").val() + '</b>' + 
+              '</div>' + 
+          '</div>' + 
+          '<div class="row">' +
+              '<div class="col-lg">' + 
+                  '<span>Captain? </span>' + 
+              '</div>' + 
+              '<div class="col-lg">' +
+                  '<b>' + $("#newPlayerCaptain").val() + '</b>' +
+              '</div>' +
+          '</div>' +
+      '</div>' +
+  '</div>').appendTo("#players");
+    
+    // reset newPlayer Card
+    $("#newPlayerName").val("")
+    $("#newPlayerSR").val("")
+    $("#newPlayerCaptain").val("No")
   }
 }
